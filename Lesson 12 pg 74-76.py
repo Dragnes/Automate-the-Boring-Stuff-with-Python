14c8165a66c9eae457dfa299579c53eb7fa96b0e
Dhruv Sengar <sengardhruv101@gmail.com>
2018-03-16 15:41:41 -0400

    Update Lesson 12 pg 74-76.py

--- Lesson 12 pg 74-76.py
+++ Lesson 12 pg 74-76.py
@@ -44,16 +44,3 @@ n = input('Give me a number: ')
 while n != 1:
     n = collatz_sequence(int(n))
     
-
-def collatz(n):
-    seq = [n]
-    while n > 1:
-        if n%2 == 1:
-            n = n*3 + 1
-            seq.append(n)
-        else:
-            n = n/2
-            seq.append(n)
-    return seq
-                   
-print (collatz(7))
